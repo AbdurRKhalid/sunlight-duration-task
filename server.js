00cc98d7fed96ebf6395a57324ebbcc5821ded1b
Abdur Rehman Khalid <akhalid@sandsmedia.com>
2024-06-23 13:15:42 +0200

    Refactored the Code.

--- server.js
+++ server.js
@@ -4,23 +4,49 @@ import {findLongestDayLightDurationPlace, readJSONFile} from "./utility/helper.j
 let app = express();
 
 app.get('/longest-sunlight-locations', async (req, res) => {
-  let response = {};
-  let jsonData = await readJSONFile('./coding_challenge_locations.json');
-  let dates = ['2024-01-01', '2024-04-01', '2024-07-01', '2024-11-01']
-  for(let date of dates) {
-    let result = await findLongestDayLightDurationPlace(jsonData, date);
-    response[date] = result;
-    console.log(result);
+  try {
+    let response = {};
+    const jsonData = await readJSONFile('./coding_challenge_locations.json');
+    const dates = ['2024-01-01', '2024-04-01', '2024-07-01', '2024-11-01']
+    for(const date of dates) {
+      response[date] = await findLongestDayLightDurationPlace(jsonData, date);
+    }
+    res.send({
+      message: 'Successfully processed the request.',
+      status: 200,
+      success: true,
+      data: response
+    });
+  } catch (error) {
+    res.send({
+      message: 'Failed to process the request.',
+      status: 500,
+      success: false,
+      error: error
+    });
   }
-  res.send(response);
 });
 
 app.get('/longest-sunlight-location/:date', async (req, res) => {
-  let response = {};
-  let jsonData = await readJSONFile('./coding_challenge_locations.json');
-  let date = req.params.date;
-  response[date] = await findLongestDayLightDurationPlace(jsonData, date);
-  res.send(response);
+  try {
+    let response = {};
+    const jsonData = await readJSONFile('./coding_challenge_locations.json');
+    const date = req.params.date;
+    response[date] = await findLongestDayLightDurationPlace(jsonData, date);
+    res.send({
+      message: 'Successfully processed the request.',
+      status: 200,
+      success: true,
+      data: response
+    });
+  } catch (error) {
+    res.send({
+      message: 'Failed to process the request.',
+      status: 500,
+      success: false,
+      error: error
+    });
+  }
 });
 
 let server = app.listen(3000, () => {
--- utility/helper.js
+++ utility/helper.js
@@ -6,31 +6,35 @@ const readJSONFile = async (filePath) => {
 }
 
 const findLongestDayLightDurationPlace = async (locationsData, date) => {
-  let processedLocations = [];
-  let totalLocations = locationsData.length;
-  let processedLocationsCount = 0;
-  for (let location of locationsData) {
-    let latitude = location.latitude;
-    let longitude = location.longitude;
-    let formattedDate = moment(date).format('YYYY-MM-DD');
-    let url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&timezone=UTC&date=${formattedDate}`;
-    let response = await fetch(url);
-    let jsonData = await response.json();
-    if (jsonData.results.sunrise != null && jsonData.results.sunset != null) {
-      location.sunLightDurationMinutes = getSunlightDurationByMinutes(jsonData.results.sunrise, jsonData.results.sunset);
-      processedLocations.push(location);
-      processedLocationsCount++;
-      console.log('Processed: ', processedLocationsCount);
+  try {
+    let processedLocations = [];
+    const totalLocations = locationsData.length;
+    let processedLocationsCount = 0;
+    for (let location of locationsData) {
+      const latitude = location.latitude;
+      const longitude = location.longitude;
+      const formattedDate = moment(date).format('YYYY-MM-DD');
+      const url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&timezone=UTC&date=${formattedDate}`;
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      if (jsonData.results.sunrise != null && jsonData.results.sunset != null) {
+        location.sunLightDurationMinutes = getSunlightDurationByMinutes(jsonData.results.sunrise, jsonData.results.sunset);
+        processedLocations.push(location);
+        processedLocationsCount++;
+        console.log('Processed: ', processedLocationsCount);
+      }
     }
+    const sortedLocations = _.orderBy(processedLocations, ['sunLightDurationMinutes'], ['desc']);
+    return sortedLocations[0];
+  } catch (error) {
+    throw new Error(error);
   }
-  let sortedLocations = _.orderBy(processedLocations, ['sunLightDurationMinutes'], ['desc']);
-  return sortedLocations[0];
 }
 
 const getSunlightDurationByMinutes = (sunrise, sunset) => {
-  let formattedSunrise = moment(sunrise, 'hh:mm:ss A').format('HH:mm:ss');
-  let formattedSunset = moment(sunset, 'hh:mm:ss A').format('HH:mm:ss');
-  let duration = moment.duration(moment(formattedSunset, 'HH:mm:ss').diff(moment(formattedSunrise, 'HH:mm:ss')));
+  const formattedSunrise = moment(sunrise, 'hh:mm:ss A').format('HH:mm:ss');
+  const formattedSunset = moment(sunset, 'hh:mm:ss A').format('HH:mm:ss');
+  const duration = moment.duration(moment(formattedSunset, 'HH:mm:ss').diff(moment(formattedSunrise, 'HH:mm:ss')));
   return Math.abs(duration.asMinutes());
 }
 
